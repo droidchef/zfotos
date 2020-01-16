7ec244db6d12e55ac645784cf2fb8690916d4499
Ishan Khanna <droidchefofficial@gmail.com>
2020-01-16 19:19:26 +0100

    core: Makes RetrofitFactory dependent on OkHttpClient and Gson
    This allows us to inject both these things via DI.

--- app/src/main/java/co/droidchef/zfotos/networking/RetrofitFactory.kt
+++ app/src/main/java/co/droidchef/zfotos/networking/RetrofitFactory.kt
@@ -1,16 +1,15 @@
 package co.droidchef.zfotos.networking
 
-import com.google.gson.GsonBuilder
+import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitFactory(private val networkFactory: NetworkFactory) {
+class RetrofitFactory(private val okHttpClient: OkHttpClient, private val gson: Gson) {
 
-    private fun create(baseUrl: String, okHttpClient: OkHttpClient): Retrofit {
-        val gson = GsonBuilder()
-        val converterFactory = GsonConverterFactory.create(gson.create())
+    private fun create(baseUrl: String, okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+        val converterFactory = GsonConverterFactory.create(gson)
         val callAdapterFactory = RxJava2CallAdapterFactory.create()
 
         return Retrofit.Builder().apply {
@@ -24,7 +23,8 @@ class RetrofitFactory(private val networkFactory: NetworkFactory) {
     val retrofitRandomUserApi: Retrofit by lazy {
         return@lazy create(
             RANDOM_USER_API_BASE_URL,
-            networkFactory.okHttpClient
+            okHttpClient,
+            gson
         )
     }
 
--- app/src/test/java/co/droidchef/zfotos/networking/RetrofitFactoryTest.kt
+++ app/src/test/java/co/droidchef/zfotos/networking/RetrofitFactoryTest.kt
@@ -1,5 +1,6 @@
 package co.droidchef.zfotos.networking
 
+import com.google.gson.Gson
 import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -7,11 +8,12 @@ import org.junit.Test
 class RetrofitFactoryTest {
 
     private val networkFactory = mockk<NetworkFactory>(relaxed = true)
+    private val gson = mockk<Gson>(relaxed = true)
 
     @Test
     fun `verify random user api retrofit instance has correct base url`() {
 
-        val retrofit = RetrofitFactory(networkFactory).retrofitRandomUserApi
+        val retrofit = RetrofitFactory(networkFactory.okHttpClient, gson).retrofitRandomUserApi
 
         assertEquals(RetrofitFactory.RANDOM_USER_API_BASE_URL, retrofit.baseUrl().toString())
 
