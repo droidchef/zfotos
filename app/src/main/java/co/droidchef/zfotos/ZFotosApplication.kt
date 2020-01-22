ffd1a23f34239b3b4d73edb9362e965a48bb04ee
Ishan Khanna <droidchefofficial@gmail.com>
2020-01-22 15:14:20 +0100

    feat: Butler re-defined
    This is a fairly large commit and here is what it does:
    1. It moves away the implementation to orchestrate requests from an ExecutorService to RxJava which is much more readable.
    2. It now extracts many dependencies out of the Butler class namely; ImageCache, DeliveryManager, Downloader and Context
    This also means that we don't need to use an object Singleton for Butler but we can build an object using or DI framework (KOIN) which also makes it easier to test our individual components that comprise Butler.
    3. It adds support for Queuing requests in case network is not available.
    4. It adds support for Processing the Queued requests when network returns and then show the images on the corresponding views.

--- app/src/main/java/co/droidchef/zfotos/ZFotosApplication.kt
+++ app/src/main/java/co/droidchef/zfotos/ZFotosApplication.kt
@@ -1,15 +1,7 @@
 package co.droidchef.zfotos
 
 import android.app.Application
-import co.droidchef.zfotos.butler.Butler
-import co.droidchef.zfotos.butler.cache.DiskCache
-import co.droidchef.zfotos.butler.cache.DoubleStoryCache
-import co.droidchef.zfotos.butler.cache.MemCache
-import co.droidchef.zfotos.di.apiModule
-import co.droidchef.zfotos.di.applicationModule
-import co.droidchef.zfotos.di.networkModule
-import co.droidchef.zfotos.di.viewModelModule
-import com.jakewharton.disklrucache.DiskLruCache
+import co.droidchef.zfotos.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -25,17 +17,6 @@ class ZFotosApplication : Application() {
             modules(listOf(apiModule, networkModule, applicationModule, viewModelModule))
         }
 
-        Butler.setImageCache(
-            DoubleStoryCache(
-                MemCache(Runtime.getRuntime().maxMemory()),
-                DiskCache(DiskLruCache.open(this.cacheDir, 1, 1, 10 * 1024 * 1024))
-            )
-        )
-        registerComponentCallbacks(Butler.callbacks2)
     }
 
-    override fun onTerminate() {
-        unregisterComponentCallbacks(Butler.callbacks2)
-        super.onTerminate()
-    }
 }
--- app/src/main/java/co/droidchef/zfotos/butler/Butler.kt
+++ app/src/main/java/co/droidchef/zfotos/butler/Butler.kt
@@ -1,97 +1,40 @@
 package co.droidchef.zfotos.butler
 
-import android.annotation.SuppressLint
 import android.content.ComponentCallbacks2
+import android.content.Context
 import android.content.res.Configuration
 import android.graphics.Bitmap
-import android.os.Handler
-import android.os.Looper
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
 import android.util.Log
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import co.droidchef.zfotos.R
 import co.droidchef.zfotos.butler.cache.ImageCache
-import okhttp3.OkHttpClient
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledFuture
-import java.util.concurrent.TimeUnit
+import co.droidchef.zfotos.butler.delivery.DeliveryManager
+import co.droidchef.zfotos.butler.delivery.ImageLoadingStatusListener
+import co.droidchef.zfotos.butler.delivery.RequestStatus
+import co.droidchef.zfotos.butler.procurement.Downloader
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.lang.ref.WeakReference
+import java.util.*
 
-object Butler {
+class Butler(
+    private val imageCache: ImageCache,
+    private val deliveryManager: DeliveryManager,
+    private val downloader: Downloader<Bitmap>,
+    private val context: Context
+) {
 
-    private val executorService = Executors.newScheduledThreadPool(8)
-
-    private var imageCache: ImageCache? = null
-
-    fun setImageCache(imageCache: ImageCache) {
-        this.imageCache = imageCache
-    }
-
-    @SuppressLint("UseSparseArrays")
-    private val jobMap = HashMap<Long, ScheduledFuture<Unit>>()
-
-    private val client: OkHttpClient by lazy { OkHttpClient.Builder().build() }
-
-    private val handler by lazy { Handler(Looper.getMainLooper()) }
-
-    fun load(url: String, into: ImageView, @DrawableRes placeholder: Int) {
-
-        imageCache?.get(url)?.also {
-
-            into.setImageBitmap(it)
-
-        } ?: run {
-
-            val cb = object : ImageLoadingRequest.RequestCallback {
-                override fun onSuccess(bitmap: Bitmap) {
-                    imageCache?.put(url, bitmap)
-                    handler.post {
-                        into.setImageBitmap(bitmap)
-                    }
-                }
-
-                override fun onFailure(exception: Exception) {
-                    handler.post {
-                        into.setImageResource(placeholder)
-                    }
-                }
-
-                override fun onCancel() {
-                    // noop
-                }
-
-            }
-
-            val request = ImageLoadingRequest(url, client, cb)
-
-            val job = executorService.schedule(request, 0, TimeUnit.MILLISECONDS)
-
-            jobMap[into.tag as Long] = job
-
-        }
-    }
-
-    fun cancelLoad(imageView: ImageView) {
-        val job = jobMap[imageView.tag as Long]
-
-        job?.let {
-            it.cancel(true)
-            jobMap.remove(imageView.tag as Long)
-        }
-
-    }
-
-    fun shutDown() {
-        Log.d("ImageLoader", "is going to shut down now...")
-        if (!executorService.isShutdown) {
-            executorService.shutdownNow()
-        }
-    }
-
-    val callbacks2 = object : ComponentCallbacks2 {
+    private val componentCallbacks = object : ComponentCallbacks2 {
 
         override fun onLowMemory() {
-            Log.d("Butler", "ON LOW MEMORY")
-
-            imageCache?.trim()
+            imageCache.trim()
         }
 
         override fun onConfigurationChanged(newConfig: Configuration) {
@@ -100,11 +43,202 @@ object Butler {
 
         override fun onTrimMemory(level: Int) {
             // To keep the implementation simple for now we'll ignore the level
-            Log.d("Butler", "ON TRIM MEMORY")
-            imageCache?.clear()
+            compositeDisposable.dispose()
+            imageCache.clear()
+            pendingRequestQueue.clear()
         }
 
     }
 
+    init {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                it.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                    override fun onLost(network: Network) {
+                        super.onLost(network)
+                        networkStatus =
+                            NetworkStatus.UNAVAILABLE
+                    }
 
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        networkStatus =
+                            NetworkStatus.AVAILABLE
+                        processQueuedRequests()
+                    }
+                })
+            }
+        }
+        context.registerComponentCallbacks(componentCallbacks)
+    }
+
+    private enum class NetworkStatus {
+        AVAILABLE,
+        UNAVAILABLE
+    }
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private var networkStatus = NetworkStatus.AVAILABLE
+
+    private val pendingRequestQueue = LinkedList<Pair<WeakReference<ImageView>, String>>()
+
+    fun load(
+        url: String,
+        imageView: ImageView,
+        @DrawableRes placeholder: Int,
+        statusListener: ImageLoadingStatusListener
+    ) {
+
+        imageCache.get(url)?.also { bitmap ->
+
+            deliveryManager.deliver(bitmap, imageView)
+            deliveryManager.inform(RequestStatus.SUCCESS, statusListener)
+
+        } ?: run {
+
+            deliveryManager.deliver(placeholder, imageView)
+            deliveryManager.inform(RequestStatus.STARTED, statusListener)
+
+            val subscription = createRequestSubscription(url)
+
+            when (networkStatus) {
+
+                NetworkStatus.AVAILABLE -> {
+                    val disposable = createRequestDisposableFromSubscription(
+                        subscription,
+                        imageView,
+                        statusListener,
+                        url,
+                        placeholder
+                    )
+
+                    imageView.tag = disposable
+
+                    compositeDisposable.add(disposable)
+
+                }
+
+                NetworkStatus.UNAVAILABLE -> {
+
+                    deliveryManager.deliver(placeholder, imageView)
+                    deliveryManager.inform(RequestStatus.FAILURE, statusListener)
+
+                    pendingRequestQueue.add(Pair(WeakReference(imageView), url))
+
+                }
+            }
+        }
+    }
+
+    /**
+     * This method is used to tell Butler that the ImageView is no longer visible or has been
+     * recycled, so there is no point loading the image into it. If there is a subscription
+     * corresponding to it, please cancel it.
+     */
+    fun cancelLoad(imageView: ImageView) {
+        (imageView.tag)?.let {
+            it as Disposable
+            if (!it.isDisposed) {
+                it.dispose()
+            }
+            imageView.tag = null
+        }
+    }
+
+    fun shutDown() {
+        compositeDisposable.dispose()
+        context.unregisterComponentCallbacks(componentCallbacks)
+    }
+
+    private fun createRequestDisposableFromSubscription(
+        subscription: Single<Bitmap>,
+        into: ImageView,
+        statusListener: ImageLoadingStatusListener,
+        url: String,
+        placeholder: Int
+    ): Disposable {
+        return subscription
+            .subscribe({ bitmap ->
+
+                deliveryManager.deliver(bitmap, into)
+                deliveryManager.inform(RequestStatus.SUCCESS, statusListener)
+
+                imageCache.put(url, bitmap)
+
+            }, {
+
+                deliveryManager.deliver(placeholder, into)
+                deliveryManager.inform(RequestStatus.FAILURE, statusListener)
+
+                pendingRequestQueue.add(Pair(WeakReference(into), url))
+            })
+    }
+
+    /**
+     * This method <b>generates</b> a subscription to download an Image from a URL.
+     */
+    private fun createRequestSubscription(imageUrl: String): Single<Bitmap> {
+
+        return Single.just(imageUrl)
+            .map { downloader.download(imageUrl) }
+            .doOnDispose {
+                fetchAndCacheImage(imageUrl)
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+    }
+
+    /**
+     * This method fetches an image and caches it in the image cache.
+     * It is usually called when the image is recycled while the image was loading. Since we know
+     * that the user has scrolled past that image they won't see it now so we just attempt to
+     * download and cache that image for later use.
+     */
+    private fun fetchAndCacheImage(imageUrl: String) {
+
+        compositeDisposable.add(Single.just(imageUrl)
+            .map { downloader.download(imageUrl) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe({
+                imageCache.put(imageUrl, it)
+            }, {
+                Log.e("Butler", "Failed to download image from $imageUrl", it)
+            })
+        )
+
+    }
+
+    /**
+     * This method processes all the requests that were queued when the Network on the device
+     * was unavailable.
+     */
+    private fun processQueuedRequests() {
+
+        while (pendingRequestQueue.isNotEmpty()) {
+            pendingRequestQueue.poll()?.let { pair ->
+
+                compositeDisposable.add(Single.just(pair.second)
+                    .map { imageUrl -> downloader.download(imageUrl) }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({ bitmap ->
+                        imageCache.put(pair.second, bitmap)
+                        pair.first.get()?.apply {
+                            setImageBitmap(bitmap)
+                        }
+                    }, {
+                        pair.first.get()?.apply {
+                            setImageResource(R.drawable.loading_placeholder)
+                        }
+                    })
+                )
+            }
+        }
+
+    }
 }
